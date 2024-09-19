41bfa0791d4e8d4e030364f2da3d97ebe050a1c1
DIOGO MADEIRA DOS SANTOS <diogo.msantos7@senacsp.edu.br>
2024-09-18 21:22:09 -0300

    Save/listAll PHP

--- model/modelProducts.php
+++ model/modelProducts.php
@@ -1,6 +1,6 @@
-
 <?php
 
+require_once("../services/connectionDB.php");
 
 class modelProducts
 {
@@ -81,7 +81,59 @@ class modelProducts
         }
     }
 
-    public function update($id, $data) {}
+    public function update($id, $data) {
+        try {
+            $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+            $product_name = htmlspecialchars($data["product_name"], ENT_NOQUOTES);
+            $image = htmlspecialchars($data["image"], ENT_NOQUOTES);
+            $price = filter_var($data["price"], FILTER_SANITIZE_NUMBER_FLOAT);
+            $id_category = filter_var($data["id_category"], FILTER_SANITIZE_NUMBER_INT);
+            $id_status = filter_var($data["id_status"], FILTER_SANITIZE_NUMBER_INT);
+            $description = htmlspecialchars($data["description"], ENT_NOQUOTES);
 
-    public function delete($id) {}
+            $conn = connectionDB::connect();
+
+            $update = $conn->prepare("UPDATE tblProducts SET 
+            product_name = :product_name, 
+            image = :image, 
+            price = :price, 
+            description = :description,
+            id_category = :id_category,
+            id_status = :id_status, updated_at = NOW() WHERE id_product = :id");
+
+            $update->bindParam(":product_name", $product_name);
+            $update->bindParam(":image", $image);
+            $update->bindParam(":price", $price);
+            $update->bindParam(":description", $description);
+            $update->bindParam(":id_category", $id_category);
+            $update->bindParam(":id_status", $id_status);
+            $update->bindParam(":id", $id);
+
+            $update->execute();
+
+            return true;
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+
+
+    // Deletar um produto por  ID
+    public function delete($id) {
+        try {
+            $id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+            $conn = connectionDB::connect();
+
+            $delete = $conn->prepare("DELETE FROM tblProducts WHERE id_product = :id");
+            $delete->bindParam(":id", $id);
+            $delete->execute();
+
+            return true;
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
 }
